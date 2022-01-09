a5cac5641548367b03357f54a4faa7a039c72cd2
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-01-09 15:57:24 +0800

    hyzgaar ogow

--- index.js
+++ index.js
@@ -20,18 +20,18 @@ window.onscroll = (e) => {
 
     const fixedParentY = $penCont.closest('.block').offsetTop;
     console.log('blockY:' + fixedParentY, 'windowScroll:' + window.scrollY, 'wH: ' + windowHeight);
-    if (windowBottomY - windowHeight / 2 - window.scrollY) {;
-        console.log(windowBottomY - windowHeight / 2 - window.scrollY);
+    if (windowBottomY - windowHeight / 2 - fixedParentY >= -windowHeight && window.scrollY < windowHeight * 1.3) {;
+        console.log(windowBottomY - windowHeight / 2 - fixedParentY);
         const a = windowBottomY - windowHeight / 2 - fixedParentY;
         const $left = $penCont.querySelector('.left');
         const $right = $penCont.querySelector('.right');
         $left.style.transform = `translateX(-${a}px)`;
         $right.style.transform = `translateX(${a}px)`;
-        $ab.style.fontSize = `${window.scrollY / 50}px`
-            // if (window.scrollY > fixedParentY) {
-            //     $penCont.classList.add('fixed');
-            // } else {
-            //     $penCont.classList.remove('fixed');
-            // }
+        // $ab.style.fontSize = `${window.scrollY / 50}px`
+        // if (window.scrollY > fixedParentY) {
+        //     $penCont.classList.add('fixed');
+        // } else {
+        //     $penCont.classList.remove('fixed');
+        // }
     }
 }
