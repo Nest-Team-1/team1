7fcc7750b0944cf2b5e53a6ee06e182978263333
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-01-10 13:10:16 +0800

    done1

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const $penCont = document.querySelector('.penCont');
 const $ab = document.querySelector('.about');
 
 const TOP_GAP = 250;
-const windowHeight = window.innerHeight;
 
 window.onscroll = (e) => {
+    let windowHeight = window.innerHeight;
+    let windowWidth = window.innerWidth;
     const windowBottomY = window.scrollY + windowHeight;
     const calcWindowBottomY = windowBottomY - TOP_GAP;
 
@@ -20,16 +21,17 @@ window.onscroll = (e) => {
 
     const fixedParentY = $penCont.closest('.block').offsetTop;
     console.log('blockY:' + fixedParentY, 'windowScroll:' + window.scrollY, 'wH: ' + windowHeight);
-    if (windowBottomY - windowHeight / 2 - fixedParentY >= -windowHeight && window.scrollY < windowHeight * 1.3) {;
+    // if (windowBottomY - windowHeight / 2 - fixedParentY >= -windowHeight && window.scrollY < windowHeight * 1.3) {;
+    if (windowBottomY - windowHeight / 2 - fixedParentY >= -windowHeight && windowBottomY - windowHeight / 2 - fixedParentY < windowWidth * 0.4) {;
         console.log(windowBottomY - windowHeight / 2 - fixedParentY);
         const a = windowBottomY - windowHeight / 2 - fixedParentY;
         const $left = $penCont.querySelector('.left');
         const $right = $penCont.querySelector('.right');
-        $left.style.transform = `rotateZ( -${a}deg)`
+        // $left.style.transform = `rotateZ( -${a}deg)`
         $left.style.transform = `translateX(-${a}px)`;
         $right.style.transform = `translateX(${a}px)`;
         // $ab.style.fontSize = `${window.scrollY / 50}px`
-        if (window.scrollY > fixedParentY) {
+        if (windowBottomY - windowHeight / 2 - fixedParentY < windowWidth * 0.4) {
             $penCont.classList.add('fixed');
         } else {
             $penCont.classList.remove('fixed');
