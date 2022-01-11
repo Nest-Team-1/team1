eae3935ff6fcad2bb7ece12a79ddc8f7cb6aff55
Puujee04 <purevsuren04@gmail.com>
2022-01-11 13:26:23 +0800

    email and password signup

---
+++ login/login.js
@@ -0,0 +1,40 @@
+const $loginBtn = document.getElementById('submit-btn');
+const $registBtn = document.getElementById('regist-btn');
+const $loginField = document.querySelector('.login');
+const $signUpField = document.querySelector('.sign-up');
+const $phoneSigInField = document.querySelector('phone-sign-in');
+const $registRegisterBtn = document.getElementById('');
+const $registEmail = document.getElementById('register-email');
+const $registPassword = document.getElementById('register-password');
+const $registUsername = document.getElementById('register-name');
+const $registerRegistBtn = document.getElementById('register-regist-btn');
+$registBtn.addEventListener('click', () => {
+    $loginField.style.display = 'none';
+    $signUpField.style.display = 'flex';
+});
+
+// Бүртгүүлэх хэсэг
+$registerRegistBtn.addEventListener('click', ()=> {
+    console.log('kkk');
+    const email = $registEmail.value;
+    const password = $registPassword.value;
+    const uName = $registUsername.value;
+  firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    user.updateProfile({
+        displayName: uName
+    }).then(() => {
+        console.log('Update data...');
+    }).catch((err) => {
+        console.log(`Error:${err}`);
+    })
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+})
