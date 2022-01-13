69a5878ade9d78afc0441fe66d6521bcd93e0075
Setgel <Setgel@LEAP-16s-iMac.local>
2022-01-13 14:55:54 +0800

    mweh

--- hp-animation/index.js
+++ hp-animation/index.js
@@ -2,31 +2,38 @@ const $container = document.querySelector('.container');
 const $box1 = document.querySelector('.box1');
 const $box2 = document.querySelector('.box2');
 const $null = document.querySelector('.null');
+const $boxContainer = document.getElementById('box-container');
 
 
 const gap = 250;
-var timer;
 
 window.onscroll = (e) => {
-    if(timer){ clearTimeout(timer)}
     const windowHeight = window.innerHeight;
-    var timer = setTimeout( () => { 
-        const windowBottomY = window.scrollY + windowHeight;
-        console.log(windowBottomY);
-        const calcWithBottomY = windowBottomY - gap;
-        if(calcWithBottomY > $container.offsetTop){
-            $container.classList.add('active');
-        }else{
-            $container.classList.remove('active');
-        }
+    const windowBottomY = window.scrollY + windowHeight;
+    console.log(windowBottomY);
+    
+    const fixedY = windowHeight + windowHeight/2 + $box1.offsetHeight/2
+    if(windowBottomY > fixedY){
+        console.log(`fixed`);
+        $boxContainer.style.position = 'fixed';
+        $boxContainer.style.top = '30vh'
+    }else{
+        $boxContainer.style.position = 'inherit'
+    }
+    const boxMover = () => {
         if(windowBottomY > $box1.offsetHeight + $null.offsetHeight + gap){
-            const x = windowBottomY - ($box1.offsetHeight + $null.offsetHeight + gap);
             $box1.style.transform = `translateX(-${x}px)`
             $box2.style.transform = `translateX(${x}px)`
-        }} , 50)
-
-
-    
+        }        
+        else{
+            $box1.style.transform = `translateX(0px)`
+            $box2.style.transform = `translateX(0px)`
+        }   
+    }
+    let calcBoxX = $box1.getBoundingClientRect();
+    console.log(calcBoxX.left);
+    let x = windowBottomY - ($box1.offsetHeight + $null.offsetHeight + gap);  
+    boxMover();   
+  
 }
-
 
