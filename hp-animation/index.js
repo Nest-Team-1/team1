72fb89a86f0ae36170a78861e65bca997f665b54
Sekeee <setgelll1212@gmail.com>
2022-01-13 23:35:57 +0800

    mweh

--- hp-animation/index.js
+++ hp-animation/index.js
@@ -11,36 +11,39 @@ $box2.style.opacity = `0`
 window.onscroll = (e) => {
     const windowHeight = window.innerHeight;
     const windowBottomY = window.scrollY + windowHeight;
-    console.log(windowBottomY);
     
     if(windowBottomY > $null.offsetHeight + gap/2){
-        console.log(windowBottomY - ($null.offsetHeight + gap/2));
-        $box1.style.opacity = `0.${(windowBottomY - ($null.offsetHeight + gap/2))*3}`
-        $box2.style.opacity = `0.${(windowBottomY - ($null.offsetHeight + gap/2))*3}`
-    }
-    if(windowBottomY > $null.offsetHeight + gap){
-        $box1.style.opacity = `1`
-        $box2.style.opacity = `1`
+        opacity = windowBottomY - ($null.offsetHeight + gap/2);
+        $box1.style.opacity = `0.${opacity}`;
+        $box2.style.opacity = `0.${opacity}`;
+        console.log(opacity);
+        if(opacity > 90){
+            $box1.style.opacity = `1`;
+            $box2.style.opacity = `1`;
+        }
+    } else if(windowBottomY < $null.offsetHeight) {
+        $box1.style.opacity = `0`;
+        $box2.style.opacity = `0`;
     }
     
     const fixedY = windowHeight + windowHeight/2 + $box1.offsetHeight/2
     if(windowBottomY > fixedY){
         $boxContainer.style.position = 'fixed';
-        $boxContainer.style.top = '30vh'
+        $boxContainer.style.top = '30vh';
     }else{
-        $boxContainer.style.position = 'inherit'
+        $boxContainer.style.position = 'inherit';
     }
     const boxMover = () => {
-        if(windowBottomY > $box1.offsetHeight + $null.offsetHeight + gap && windowBottomY < window.innerWidth){
-            $box1.style.transform = `translateX(-${x}px)`
-            $box2.style.transform = `translateX(${x}px)`
+        if(windowBottomY > $box1.offsetHeight + $null.offsetHeight + gap && windowBottomY < $container.offsetWidth){
+            $box1.style.transform = `translateX(-${x}px)`;
+            $box2.style.transform = `translateX(${x}px)`;
         }     
         if(windowBottomY < $box1.offsetHeight + $null.offsetHeight + gap){
-            $box1.style.transform = `translateX(0px)`
-            $box2.style.transform = `translateX(0px)`
+            $box1.style.transform = `translateX(0px)`;
+            $box2.style.transform = `translateX(0px)`;
         }   
     }
     let calcBoxX = $box1.getBoundingClientRect();
     let x = windowBottomY - ($box1.offsetHeight + $null.offsetHeight + gap);  
-    boxMover();   
+    boxMover(); 
 }
