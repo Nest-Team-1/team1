48978533b6a703abbdff33f394390d9a4221d325
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-01-14 14:07:31 +0800

    dasda

--- homepage.js
+++
@@ -1,9 +0,0 @@
-const $backImg = document.querySelector('.container');
-
-window.onscroll = (e) => {
-    console.log(window.scrollY);
-    $backImg.style.backgroundSize = `${100+window.scrollY}%`
-        // $backImg.style.transform = `scale(${100+window.scrollY}%)`
-        // $backImg.style.transform = `${50, 84}%`
-        // transform-origin: 50% 84%;
-}
