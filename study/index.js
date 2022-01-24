a18428bac07359bf4bf8906df6111242a4dd7b0a
Setgel <Setgel@LEAP-16s-iMac.local>
2022-01-24 14:54:23 +0800

    mweh

--- study/index.js
+++ study/index.js
@@ -2,12 +2,13 @@ var canvas = document.getElementById("canvasShapes");
 var ctx = canvas.getContext("2d");
 
 //Define x and y scales
-var xScale = 50;
-var yScale = 50;
-
+var xScale = 30;
+var yScale = 20;
+canvas.height = document.querySelector('.graphic').offsetHeight;
+canvas.width = document.querySelector('.graphic').offsetWidth;
 // width and height of canvas in pixels
-var width = 815;
-var height = 483;
+var width = canvas.width;
+var height = canvas.height;
 
 // coordinates of center of canvas in pixels
 var xCenter = width / 2;
@@ -30,14 +31,14 @@ drawAxis = () => {
     ctx.stroke();
 
     // draw ticks of 1 unit (=100pixels) along x axis
-    ctx.strokeStyle = "grey";
+    ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.lineWidth = 2;
-    for (i = 1; i <= 8; i++) {
+    for (i = 1; i <= 30; i++) {
         ctx.moveTo(xCenter + i * xScale, yCenter + 5);
         ctx.lineTo(xCenter + i * xScale, yCenter - 5);
     }
-    for (i = 1; i <= 8; i++) {
+    for (i = 1; i <= 30; i++) {
         ctx.moveTo(xCenter - i * xScale, yCenter + 5);
         ctx.lineTo(xCenter - i * xScale, yCenter - 5);
     }
@@ -45,14 +46,14 @@ drawAxis = () => {
 
 
     // draw ticks of 1 unit (=50pixels) along y axis
-    ctx.strokeStyle = "grey";
+    ctx.strokeStyle = "black";
     ctx.beginPath();
     ctx.lineWidth = 2;
-    for (i = 1; i <= 4; i++) {
+    for (i = 1; i <= 20; i++) {
         ctx.moveTo(xCenter - 5, yCenter + i * yScale);
         ctx.lineTo(xCenter + 5, yCenter + i * yScale);
     }
-    for (i = 1; i <= 4; i++) {
+    for (i = 1; i <= 20; i++) {
         ctx.moveTo(xCenter - 5, yCenter - i * yScale);
         ctx.lineTo(xCenter + 5, yCenter - i * yScale);
     }
@@ -64,6 +65,8 @@ drawAxis();
 const $range1 = document.getElementById('range1');
 const $range2 = document.getElementById('range2');
 const $range3 = document.getElementById('range3');
+const $max = document.getElementById('max');
+const $min = document.getElementById('min');
 
 let a;
 let b;
@@ -76,20 +79,36 @@ var x = [];
 var xx;
 
 $range1.onchange = (e) => {
-    
     a = parseInt(e.target.value);
-    console.log(a);
+    b = parseInt($range2.value);
+    c = parseInt($range3.value);
     return rangeChange();
-
+}
+$range2.onchange = (e) => {
+    a = parseInt($range1.value);
+    b = parseInt(e.target.value);
+    c = parseInt($range3.value);
+    return rangeChange();
+}
+$range3.onchange = (e) => {
+    a = parseInt($range1.value);
+    b = parseInt($range2.value);
+    c = parseInt(e.target.value);
+    return rangeChange();
 }
 
 rangeChange = () => {
-    ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+
+    // calculate function y = x^2 from x = -3 to x = 3
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
     drawAxis();
+    ctx.restore();
     for (i = 0; i <= width + 1; i++) {
-        x[i] = a * (xCenter - i);
+        x[i] = a * (xCenter - i) ;
         xx = x[i] / xScale;
-        y[i] = -a * yScale * xx * xx;
+        y[i] = -a * yScale * xx * xx + (-yScale*b*xx)+(-yScale*c);
     }
 
     ctx.strokeStyle = "red";
@@ -100,17 +119,38 @@ rangeChange = () => {
         ctx.lineTo(x[i + 1], y[i + 1]);
     }
     ctx.stroke();
+    const $showFunc = document.querySelector('.showfunc');
+    $showFunc.innerHTML = `y = ${a}x^2 +${b}x + ${c}`;
 }
 
-$range2.onchange = (e) => {
-    b = e.target.value;
+$max.onchange = (e) => {
+    ctx.save();
+    ctx.setTransform(1, 0, 0, 1, 0, 0);
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    drawAxis();
+    ctx.restore();
+    let g = [];
+    let h = [];
+    z = parseInt(e.target.value);
+    console.log(z);
+    for(i = 0 ; i <= canvas.height/2 ; i++){
+        g[i] = z*xScale;
+        h[i] = i*yScale - canvas.height/2;
+    }
+
+    ctx.strokeStyle = "blue";
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    for(i = 0 ; i <= canvas.height ; i++){
+        ctx.moveTo(g[i] , h[i]);
+        ctx.lineTo(g[i+2] , h[i + 2]);
+    }
+    ctx.stroke();
 }
-$range3.onchange = (e) => {
-    c = e.target.value;
-}
 
 
 
 
 
-// calculate function y = x^2 from x = -3 to x = 3
+
+
