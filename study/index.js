2e598e9162611306982d8ff91da5999b744ad403
Setgel <Setgel@LEAP-16s-iMac.local>
2022-01-24 13:02:39 +0800

    mweh

--- study/index.js
+++ study/index.js
@@ -10,79 +10,107 @@ var width = 815;
 var height = 483;
 
 // coordinates of center of canvas in pixels
-var xCenter = width/2;
-var yCenter = height/2;
+var xCenter = width / 2;
+var yCenter = height / 2;
 
-// draw axes
-ctx.strokeStyle = "black";
-ctx.beginPath();
-ctx.lineWidth = 2;
+drawAxis = () => {
 
-// draw x axis
-ctx.moveTo(0,yCenter);
-ctx.lineTo(width,yCenter);
+    // draw axes
+    ctx.strokeStyle = "black";
+    ctx.beginPath();
+    ctx.lineWidth = 2;
 
-// draw y axis
-ctx.moveTo(xCenter,0);
-ctx.lineTo(xCenter,height);
-ctx.stroke();
+    // draw x axis
+    ctx.moveTo(0, yCenter);
+    ctx.lineTo(width, yCenter);
 
-// draw ticks of 1 unit (=100pixels) along x axis
-ctx.strokeStyle = "grey";
-ctx.beginPath();
-ctx.lineWidth = 2;
-for (i=1;i<=8;i++)
-{
-ctx.moveTo(xCenter+i*xScale,yCenter + 5);
-ctx.lineTo(xCenter+i*xScale,yCenter - 5);
+    // draw y axis
+    ctx.moveTo(xCenter, 0);
+    ctx.lineTo(xCenter, height);
+    ctx.stroke();
+
+    // draw ticks of 1 unit (=100pixels) along x axis
+    ctx.strokeStyle = "grey";
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    for (i = 1; i <= 8; i++) {
+        ctx.moveTo(xCenter + i * xScale, yCenter + 5);
+        ctx.lineTo(xCenter + i * xScale, yCenter - 5);
+    }
+    for (i = 1; i <= 8; i++) {
+        ctx.moveTo(xCenter - i * xScale, yCenter + 5);
+        ctx.lineTo(xCenter - i * xScale, yCenter - 5);
+    }
+    ctx.stroke();
+
+
+    // draw ticks of 1 unit (=50pixels) along y axis
+    ctx.strokeStyle = "grey";
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    for (i = 1; i <= 4; i++) {
+        ctx.moveTo(xCenter - 5, yCenter + i * yScale);
+        ctx.lineTo(xCenter + 5, yCenter + i * yScale);
+    }
+    for (i = 1; i <= 4; i++) {
+        ctx.moveTo(xCenter - 5, yCenter - i * yScale);
+        ctx.lineTo(xCenter + 5, yCenter - i * yScale);
+    }
+    ctx.stroke();
 }
-for (i=1;i<=8;i++)
-{
-ctx.moveTo(xCenter-i*xScale,yCenter + 5);
-ctx.lineTo(xCenter-i*xScale,yCenter -5);
-}
-ctx.stroke();
+drawAxis();
 
+//range
+const $range1 = document.getElementById('range1');
+const $range2 = document.getElementById('range2');
+const $range3 = document.getElementById('range3');
 
-// draw ticks of 1 unit (=50pixels) along y axis
-ctx.strokeStyle = "grey";
-ctx.beginPath();
-ctx.lineWidth = 2;
-for (i=1;i<=4;i++)
-{
-ctx.moveTo(xCenter - 5,yCenter+i*yScale);
-ctx.lineTo(xCenter + 5,yCenter+i*yScale);
-}
-for (i=1;i<=4;i++)
-{
-ctx.moveTo(xCenter - 5,yCenter-i*yScale);
-ctx.lineTo(xCenter + 5,yCenter-i*yScale);
-}
-ctx.stroke();
-
+let a;
+let b;
+let c;
 
 // graph function
-ctx.translate(xCenter,yCenter);
-var y=[ ];
-var x=[ ];
+ctx.translate(xCenter, yCenter);
+var y = [];
+var x = [];
 var xx;
 
-// calculate function y = x^2 from x = -3 to x = 3
-for (i=0;i<=width+1;i++)
-{
-x[i] = canvas.width/2 - i;
-xx=x[i]/xScale;
-y[i] = -yScale*Math.pow(xx,2); 
-//console.log(x, xx, y)
-}
-console.log(x);
+$range1.onchange = (e) => {
+    
+    a = parseInt(e.target.value);
+    console.log(a);
+    return rangeChange();
 
-ctx.strokeStyle = "red";
-ctx.beginPath();
-ctx.lineWidth = 2;
-for (i=0;i<=canvas.width;i++)
-{
-ctx.moveTo(x[i],y[i]);
-ctx.lineTo(x[i+1],y[i+1]);
 }
-ctx.stroke();
+
+rangeChange = () => {
+    ctx.clearRect(-canvas.width / 2, -canvas.height / 2, canvas.width, canvas.height);
+    drawAxis();
+    for (i = 0; i <= width + 1; i++) {
+        x[i] = a * (xCenter - i);
+        xx = x[i] / xScale;
+        y[i] = -a * yScale * xx * xx;
+    }
+
+    ctx.strokeStyle = "red";
+    ctx.beginPath();
+    ctx.lineWidth = 2;
+    for (i = 0; i <= canvas.width; i++) {
+        ctx.moveTo(x[i], y[i]);
+        ctx.lineTo(x[i + 1], y[i + 1]);
+    }
+    ctx.stroke();
+}
+
+$range2.onchange = (e) => {
+    b = e.target.value;
+}
+$range3.onchange = (e) => {
+    c = e.target.value;
+}
+
+
+
+
+
+// calculate function y = x^2 from x = -3 to x = 3
