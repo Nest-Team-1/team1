318e0150221aca177118a87e4ebef3edcde592b0
Ds21saikhnaa <dashmandalsaikhanbileg@gmail.com>
2022-01-27 14:40:29 +0800

    saihnaa

--- inverseFunction/reverse_function.js
+++ inverseFunction/reverse_function.js
@@ -261,18 +261,46 @@ const dra = (sawBtn = () => {
 const $sambar = document.getElementById('sambar'); 
 const $formulas = document.getElementById('formulas');
 const $forum = document.getElementById('forum');
-const $hicheel = document.getElementById('hicheel');
-const $before = document.getElementById('before');
+const $start = document.getElementById('start');
 
-$hicheel.onclick = () => {
-    console.log('hello');
+
+const $linear=document.getElementById('linear')
+const $kwadrat=document.getElementById('kwadrat')
+const $maxmin=document.getElementById('maxmin')
+const $oddeven=document.getElementById('oddeven')
+const $inserse=document.getElementById('inverse')
+const $register=document.getElementById('signup')
+const $login=document.getElementById('login')
+$login.onclick=()=>{
+    window.location='../login/index.html'
 }
-$before.onclick = () => {
-    window.location = "../oddEvenFunction/index.html";
+$register.onclick=()=>{
+    window.location='../register/index.html'
 }
+$inserse.onclick=()=>{
+    window.location='../inversefunction/reverse-function.html'
+}
+$oddeven.onclick=()=>{
+    window.location='../oddevenfunction/index.html'
+}
+$maxmin.onclick=()=>{
+    window.location='../maxminfunction/index.html'
+}
+$kwadrat.onclick=()=>{
+    window.location='../kwadratfunction/kwadrat_function.html'
+}
+$linear.onclick=()=>{
+    window.location= '../linearfunction/linear_function.html'
+}
+$start.onclick = () => {
+    console.log('dsd');
+    window.location = '../whatIsFunction/what_is_function.html';
+}
+
+
 
 $sambar.onclick = () => {
-    window.location = "../sambar/draw.html";
+    window.location = "../sambar/draw.html"; 
 }
 $formulas.onclick = () => {
     window.location = '../formulas/index.html';
