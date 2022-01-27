cc8f05e021e1e2115d33a9ef6539c819564a0c47
javha97 <jjavkhlan123@gmail.com>
2022-01-27 14:29:33 +0800

    homepage

--- Homepage/index.js
+++ Homepage/index.js
@@ -81,16 +81,48 @@ const $sambar = document.getElementById('sambar');
 const $formulas = document.getElementById('formulas');
 const $forum = document.getElementById('forum');
 const $start = document.getElementById('start');
+var $start1=document.querySelector('.start')
 const $negdeh = document.getElementById('negdeh');
 const $harah = document.getElementById('harah');
-const $hicheel = document.getElementById('hicheel');
 
-$hicheel.onclick = () => {
-    console.log('hello');
+
+const $linear=document.getElementById('linear')
+const $kwadrat=document.getElementById('kwadrat')
+const $maxmin=document.getElementById('maxmin')
+const $oddeven=document.getElementById('oddeven')
+const $inserse=document.getElementById('inverse')
+const $register=document.getElementById('signup')
+const $login=document.getElementById('login')
+$start1.onclick=()=>{
+    console.log('hi')
+    window.location='../whatIsFunction/what_is_function.html';
+}
+$login.onclick=()=>{
+    window.location='../login/index.html'
+}
+$register.onclick=()=>{
+    window.location='../register/index.html'
+}
+$inserse.onclick=()=>{
+    window.location='../inversefunction/reverse-function.html'
+}
+$oddeven.onclick=()=>{
+    window.location='../oddevenfunction/index.html'
+}
+$maxmin.onclick=()=>{
+    window.location='../maxminfunction/index.html'
+}
+$kwadrat.onclick=()=>{
+    window.location='../kwadratfunction/kwadrat_function.html'
+}
+$linear.onclick=()=>{
+    window.location= '../linearfunction/linear_function.html'
 }
 
+
+
 $sambar.onclick = () => {
-    window.location = "../sambar/draw.html";
+    window.location = "../sambar/draw.html"; 
 }
 $formulas.onclick = () => {
     window.location = '../formulas/index.html';
@@ -99,6 +131,7 @@ $forum.onclick = () => {
     window.location = '../forum/index.html';
 }
 $start.onclick = () => {
+    console.log('dsd');
     window.location = '../whatIsFunction/what_is_function.html';
 }
 $negdeh.onclick = () => {
